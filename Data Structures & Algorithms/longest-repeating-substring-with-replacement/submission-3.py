16a77faf00dab8fbaa07a80fcc8a717792c86d89
Anmol Rawat <79262776+Anmol-PROgrammar@users.noreply.github.com>
2026-04-11 19:34:27 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        myMap = {i:0  for i in range(26)}
+        l = 0
+        r = 0
+        res = 0
+        while r < len(s):
+            # insert element 
+            myMap[ord(s[r]) - ord('A')] += 1
+            # find max freq in the window
+            hFreq = max(myMap.values())
+
+            # check if current window size - highest ferq <= k(replacement allowed) 
+            # if yes increase window to right 
+            # if no decrease window from left untill logic is valid again 
+            if ((r - l + 1) - hFreq) <= k:
+                res = max(res,r-l+1)
+            else:
+                # remove left element from dict
+                myMap[ord(s[l]) - ord('A')] -= 1
+                l+=1
+            r+=1
+
+        return res

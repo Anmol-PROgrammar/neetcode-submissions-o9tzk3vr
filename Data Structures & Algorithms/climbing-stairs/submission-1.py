0300ea188e1f10546491f96175cba8b342285df6
Anmol Rawat <79262776+Anmol-PROgrammar@users.noreply.github.com>
2026-06-21 22:25:38 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        sum =  [0] * (n + 1);
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        sum[1] = 1
+        sum[2] = 2
+        for i in range(3,n+1):
+            sum[i] = sum[i - 1] + sum[i - 2]
+        return sum[n]
